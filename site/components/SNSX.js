7226ae0936842458f48acb66012c223a2e312479
StevenIseki <stevenisekimartin@gmail.com>
2017-01-28 01:54:25 +1100

    v 2

--- site/components/SNSX.js
+++ site/components/SNSX.js
@@ -9,16 +9,26 @@ export default class Projects extends Component {
   render() {
     return (
       <div styleName='Wrapper'>
-        <div styleName='Column'>
-          <p styleName='Heading'>SNSX</p>
+        <div styleName='ColumnForum'>
+          <a styleName='Forum' href='http://n4m.snsx.sx'>SNSX Forum</a>
         </div>
-        <div styleName='Column'>
+        <div styleName='ColumnHeading'>
+          <p styleName='Heading'>SNSX</p>
+          <p styleName='Description'>
+            Social Netball Sexual Experience is Airknight and Apple McStrudel escaped from the wreckage of Social Netball 🏐🏐🏐 with a
+            clear goal, to play some rocking tunes and avoid any big boom booms.
+            With influences as varied as The Anniversary and The Lemonheads this new sound is set
+            to put your speakers into sensual overdrive 🎹🎹🎹
+          </p>
+        </div>
+        <div styleName='ColumnTriangle'>
           <Triangle />
         </div>
-        <div styleName='ColumnLast'>
-          <p styleName='SubHeading'>Social Netball</p>
-          <img styleName='Netball' src='http://i.imgur.com/iHVkevD.png' />
-          <p styleName='SubHeading'>Sexual Experience</p>
+
+        <div styleName='ColumnEmbedPlace' />
+
+        <div styleName='ColumnEmbed'>
+          <iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/304780277&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>
         </div>
       </div>
     )
